d573a4dfd1491dc34321c07ef7d65b70353ef60f
SulemanKhan01 <sulemanmusaddiq2105@gmail.com>
2026-07-09 17:56:38 +0500

    Added extractor to extract the Pdf text

---
+++ src/extractor.py
@@ -0,0 +1,17 @@
+import pdfplumber
+
+def extract_from_pdf(pdf_path: str):
+    all_text = []
+
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                all_text.append(page_text)
+
+    return(all_text)
+        
+
+text =extract_from_pdf("C:/Users/Suleman/Desktop/ab_ark/PitchCraft/data/raw_pdfs/POC_Proposal.pdf")
+
+print(text)
