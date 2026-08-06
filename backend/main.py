3030aded0e1e3aff3c6c5fd7eee153652812a675
SulemanKhan01 <sulemanmusaddiq2105@gmail.com>
2026-08-06 18:37:22 +0500

    Recent Conversation Bar Added

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,13 @@ app = FastAPI(title="PitchCraft API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
